b824db9687c5a51a8775c01a7488a77d9a8387e1
Danilova Elena <Danilova Elena>
2016-11-12 22:36:39 +0300

    correctind code

--- scripts/app.js
+++ scripts/app.js
@@ -1,43 +1,114 @@
 {
-    const req = new Request('https://newsapi.org/v1/articles?source=bbc-news&apiKey=7a81c3436d174f13975ddaeac8184f54');
+    class Loader {
+        constructor (link) {
+            this.link = link;
+            this.apiKey = '';
+        };
+        load() {
+            this.getApiKey();
+            return fetch(new Request(`${this.link}&apiKey=${this.apiKey}`))
+                .then(response => response.json())
+                .then(data => data.articles)
+                .catch(error => error);
+        };
 
+        getApiKey() {
+            const locationString = window.location.search.substring(1).split('=');
+            if (locationString && locationString[0] === 'apiKey') {
+                this.apiKey = locationString[1];
+            }
+        };
+    }
 
-    fetch(req)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error(response.statusText)
-            }
-        })
-        .then(data => data.articles)
-        .then(articles => {
-            for (const article of articles) {
-                const div = document.createElement('div');
-                div.classList.add('article-item');
-                div.innerHTML = `
-                    <h2 class="article-title">${article.title}</h2>
-                    <img class="article-image" src="${article.urlToImage}" alt="awesome picture"/>
-                    <p class="article-description">${article.description}</p>
-                    <a class="article-link" href="${article.url}" target="_blank">Read more...</a>
-                    <div class="article-additional-inf">
-                        <div class="article-author">
-                            <i class="fa fa-user-o" aria-hidden="true"></i>
-                            <span>${article.author}</span>
-                        </div>
-                        <div class="article-date">
-                            <i class="fa fa-clock-o" aria-hidden="true"></i>
-                            <span>${new Date(article.publishedAt).toLocaleDateString()}</span>
-                        </div>
-                    </div>`;
-                document.getElementsByClassName('article-block').item(0).appendChild(div);
-            }
-        })
-        .catch(error => {
-                const div = document.createElement('div');
-                div.classList.add('error');
-                div.innerHTML = `Failed: ${error}`;
-                document.getElementsByClassName('article-block').item(0).appendChild(div);
-            }
-        );
+    class Renderer {
+        render() {
+            const articleBlock = document.getElementsByClassName('article-block').item(0);
+            const articleBlockFragment = document.createDocumentFragment();
+            const requestLink = 'https://newsapi.org/v1/articles?source=bbc-news';
+
+            const loader = new Loader(requestLink);
+
+            loader.load().then(articles => {
+                for (const article of articles) {
+                    articleBlockFragment.appendChild(this.createArticleItemBlock(article));
+                };
+
+                articleBlock.appendChild(articleBlockFragment);
+            }).catch(error => {
+                articleBlock.appendChild(this.createErrorBlock(error));
+            })
+        };
+
+        createArticleItemBlock(article) {
+            const articleItemBlock = document.createElement('div');
+            articleItemBlock.classList.add('article-item');
+
+            const articleTitle = document.createElement('h2');
+            articleTitle.classList.add('article-title');
+            articleTitle.innerText = article.title;
+
+            const articleImg = document.createElement('img');
+            articleImg.classList.add('article-image');
+            articleImg.setAttribute('src', article.urlToImage);
+            articleImg.setAttribute('alt', 'awesome picture');
+
+            const articleDescription = document.createElement('p');
+            articleDescription.classList.add('article-description');
+            articleDescription.innerText = article.description;
+
+            const articleLink = document.createElement('a');
+            articleLink.classList.add('article-link');
+            articleLink.setAttribute('href', article.url);
+            articleLink.setAttribute('target', '_blank');
+            articleLink.innerText = 'Read more...';
+
+            const additionalInfBlock = document.createElement('div');
+            additionalInfBlock.classList.add('article-additional-inf');
+
+            const articleAuthorBlock = document.createElement('div');
+            articleAuthorBlock.classList.add('article-author');
+            const authorIcon = document.createElement('i');
+            authorIcon.classList.add('fa');
+            authorIcon.classList.add('fa-user-o');
+            authorIcon.setAttribute('aria-hidden', true);
+            const articleAuthor = document.createElement('span');
+            articleAuthor.innerText = article.author;
+            articleAuthorBlock.appendChild(authorIcon);
+            articleAuthorBlock.appendChild(articleAuthor);
+
+            const articleDateBlock = document.createElement('div');
+            articleDateBlock.classList.add('article-date');
+            const dateIcon = document.createElement('i');
+            dateIcon.classList.add('fa');
+            dateIcon.classList.add('fa-clock-o');
+            dateIcon.setAttribute('aria-hidden', true);
+            const articleDate = document.createElement('span');
+            articleDate.innerText = new Date(article.publishedAt).toLocaleDateString();
+
+            articleDateBlock.appendChild(dateIcon);
+            articleDateBlock.appendChild(articleDate);
+
+            additionalInfBlock.appendChild(articleAuthorBlock);
+            additionalInfBlock.appendChild(articleDateBlock);
+
+            articleItemBlock.appendChild(articleTitle);
+            articleItemBlock.appendChild(articleImg);
+            articleItemBlock.appendChild(articleDescription);
+            articleItemBlock.appendChild(articleLink);
+            articleItemBlock.appendChild(additionalInfBlock);
+
+            return articleItemBlock;
+        };
+
+        createErrorBlock(error) {
+            const errorBlock = document.createElement('div');
+            errorBlock.classList.add('error');
+            errorBlock.innerText = `Failed: ${error}`;
+
+            return errorBlock;
+        };
+    }
+
+    const renderer = new Renderer();
+    renderer.render();
 };
